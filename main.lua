7f1c72785650f6fcb40e118b13e4214842970509
Damoon Imani <damoonimani@pm.me>
2022-11-06 22:06:14 +0330

    function load and update added to main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+function love.load()
+  message = "Hello World"
+end
+
+function love.update(dt)
+    
+end
+
 function love.draw()
-	love.graphics.print("Hello World", 400, 300)
+	love.graphics.print(message, 400, 300)
 end
